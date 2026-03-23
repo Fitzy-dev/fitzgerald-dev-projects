cec50bb926add0bbb55731f7eeb5610c6a486c84
Fitzy-dev <jordanfitz531@gmail.com>
2026-03-22 22:41:35 -0400

    Add is_senior method to Student class

--- python/projects/School-Manager/student.py
+++ python/projects/School-Manager/student.py
@@ -8,3 +8,6 @@ class Student(Person):
     def display_info(self): # Override the display_info method to include the student's grade
         super().display_info() # Call the display_info method of the parent class to display name and age
         print(f"Student: {self.name}, Age: {self.age}, Grade: {self.grade}")
+    
+    def is_senior(self): # Method to check if the student is a senior (12th grade)
+        return self.grade == "12th Grade"
