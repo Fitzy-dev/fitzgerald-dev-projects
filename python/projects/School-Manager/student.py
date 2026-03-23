cfc597f22b7e40536711b66937d241c1744b1e3f
Fitzy-dev <jordanfitz531@gmail.com>
2026-03-22 22:26:10 -0400

    Add Student class extending Person class

---
+++ python/projects/School-Manager/student.py
@@ -0,0 +1,10 @@
+from person import Person
+
+class Student(Person):
+    def __init__(self, name, age, grade): # Constructor to initialize the student's name, age, and grade
+        super().__init__(name, age) # Call the constructor of the parent class (Person) to initialize name and age
+        self.grade = grade
+
+    def display_info(self): # Override the display_info method to include the student's grade
+        super().display_info() # Call the display_info method of the parent class to display name and age
+        print(f"Student: {self.name}, Age: {self.age}, Grade: {self.grade}")
