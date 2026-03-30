ee340435dd9acf5f728cd1d0fb8f51009f2f7f19
Fitzy-dev <jordanfitz531@gmail.com>
2026-03-30 12:01:58 -0400

    Add square function and main program

---
+++ cpp/projects/Square-Project/square.cpp
@@ -0,0 +1,18 @@
+// Jordan Fitzgerald
+// 3/28/2026
+// Square Project
+
+#include <iostream>
+using namespace std;
+
+int square(int x)
+{
+    return x * x;   
+}
+
+int main()
+{
+    int num = 6;
+    cout << "The square of " << num << " is: " << square(num) << endl;
+    return 0;
+}
